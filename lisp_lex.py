18862e4eb71120e857bc1c7184743967840eea04
gcmurillo <gcmurillo@outlook.com>
2018-06-30 16:05:38 -0500

    evaluate lexer

--- lisp_lex.py
+++ lisp_lex.py
@@ -1,6 +1,31 @@
 import ply.lex as lex
 
-# reserver words
+# tokens
+tokens = [
+	'PLUS', # +
+	'MINUS', # - 
+	'TIMES', # *
+	'DIVIDED', # /
+	'LPAREN', # (
+	'RPAREN', # )
+	'SYMBOLS', # variable names
+	'COMMENT',  # ; cualquier cosa
+	'NUMBER',
+	'COMILLA_SIMPLE',
+]
+
+t_PLUS = r'\+'
+t_MINUS = r'-'
+t_TIMES = r'\*'
+t_DIVIDED = r'/'
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+t_SYMBOLS = r'[a-z]\w*'
+t_COMMENT = r';\s?\w[\w\s]*\n'
+t_COMILLA_SIMPLE = r"\'"
+
+
+# reserved words
 reserved = {
 	'setq': "SETQ",
 	'defun': "DEFUN", 
@@ -25,9 +50,7 @@ reserved = {
 	'null': "NULL", 
 	'lambda':"LAMBDA",
 	'defmacro':"DEFMACRO",
-	'write-line':"WRITE-LINE",
 	'typep':"TYPEP",
-	'type-of':"TYPE-OF",
 	'defvar':"DEFVAR",
 	'write':"WRITE",
 	'prog':"PROG",
@@ -37,38 +60,44 @@ reserved = {
 	'when': "WHEN",
 	'case':"CASE",
 	'loop': "LOOP",
-	'loop for':"LOOP FOR",
 	'from': "FROM",
 	'to': "TO",
 	'do': "DO",
 	'dotimes':"DOTIMES"
 }
 
-# tokens
-tokens = [
-	'PLUS', # +
-	'MINUS', # - 
-	'TIMES', # *
-	'DIVIDED', # /
-	'LPAREN', # (
-	'RPAREN', # )
-	'SYMBOLS', # variable names
-	'COMMENT',  # ; cualquier cosa
-	'NUMBER',
-	'COMILLA_SIMPLE'
-]
-
-t_PLUS = r'\+'
-t_MINUS = r'-'
-t_TIMES = r'*'
-t_DIVIDED = r'/'
-t_LPAREN = r'\('
-t_RPAREN = r'\)'
-t_SYMBOLS = r'[a-z]\w*'
-t_COMMENT = r';\s?\w[\w\s]*\n'
-t_COMILLA_SIMPLE = r"\'"
+tokens = tokens + list(reserved.values())
 
 def t_NUMBER(t):
 	r'\d+'
 	t.value = int(t.value)
 	return t
+
+def t_newline(t):
+	r'\n+'
+	t.lexer.lineno += len(t.value)
+
+#TODO: validar variables con token SYMBOLS no ID
+def t_ID(t):
+	r'([a-z]+|&optional|&rest)'
+	if t.value in reserved:
+    		t.type = reserved[t.value]
+	return t
+
+t_ignore = ' '
+
+def t_error(t):
+	print("Incorrect character '%s'" % t.value[0])
+	t.lexer.skip(1)
+
+lexer = lex.lex()
+
+code = "(lambda (x) (* x x)"
+
+lexer.input(code)
+
+while True:
+	tok = lexer.token()
+	if not tok:
+		break
+	print(tok)
