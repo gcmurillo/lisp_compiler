1a285127a850bc54d58402fdde7992c46721e682
gcmurillo <gcmurillo@outlook.com>
2018-07-04 19:16:10 -0500

    fixing bugs

--- lisp_parser.py
+++ lisp_parser.py
@@ -16,45 +16,47 @@ def p_expresion_lambda(p):
     print("Correct!")
 
 def p_body(p):
-	"""body : LPAREN expresion RPAREN 
-			| factor
-			| expresion body """
+	""" body : expresion 
+			 | expresion body """
 
 def p_expresion(p):
 	""" expresion : LPAREN expresion RPAREN 
-				  | operator factor expresion 
-				  | operator expresion expresion
-				  | operator subexpresion subexpresion
-				  | operator expresion expresion expresion
-				  | ifs
-				  | factor """
+				  | LPAREN operator factor_list RPAREN
+				  | ifs """
 
 def p_subexpresion(p):
 	""" subexpresion : LPAREN factor factor RPAREN 
-					  | """
+					 | """
+
 def p_ifs(p):
 	""" ifs : IF booleans expresion expresion 
-		| IF expresion expresion expresion
-		| IF expresion expresion """
+		    | IF expresion expresion expresion
+		    | IF expresion expresion """
 
 def p_symbol_list(p):
-    """symbol_list : SYMBOL
-                   | symbol_list SYMBOL"""
+    """ symbol_list : SYMBOL
+                    | symbol_list SYMBOL"""
+
+def p_factor_list(p):
+    """ factor_list : factor 
+                    | factor_list factor"""
 
 def p_factor(p):
-    """factor : SYMBOL 
-              | NUMBER 
-              | DECIMAL """
+    """ factor : SYMBOL 
+               | NUMBER 
+               | DECIMAL """
 
 def p_operator(p):
-	 """operator : PLUS 
-              | MINUS 
-              | TIMES
-              | DIVIDED 
-	            | EQUAL"""
+	""" operator : PLUS 
+                 | MINUS 
+                 | TIMES
+                 | DIVIDED 
+	             | EQUAL"""
+
 def p_booleans(p):
 	""" booleans : T 
-              | NIL"""
+                 | NIL"""
+
 def p_error(p):
     print("Syntax error!")
 
